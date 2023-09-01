225ff9505ae1852ed78bfda8f48d4d573e56a929
Curve <fynnbwdt@gmail.com>
2023-09-01 16:42:22 +0200

    fix: remove self parameter in callbacks

--- mod_api.d.ts
+++ mod_api.d.ts
@@ -13,7 +13,7 @@ interface mod {
     enable(value: boolean): void;
 }
 
-type restore_fn = () => void;
+type restore_fn = (this: void) => void;
 
 declare const mod_api: {
     logger: {
@@ -36,8 +36,8 @@ declare const mod_api: {
 
     hooks: {
         /** @noSelf */
-        detour(module: object, func: string, detour: (original: any, ...args: any[]) => any): restore_fn;
+        detour(module: object, func: string, detour: (this: void, original: any, ...args: any[]) => any): restore_fn;
         /** @noSelf */
-        intercept_require(module: string, callback: (module: object) => void): restore_fn;
+        intercept_require(module: string, callback: (this: void, module: object) => void): restore_fn;
     },
 }
